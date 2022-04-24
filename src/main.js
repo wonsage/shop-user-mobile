655a66debc5bf690f964d680d1b9ad6dc2961880
wonSage <34784300+wonsage@users.noreply.github.com>
2022-04-24 16:37:10 +0800

    preparing complete

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'amfe-flexible'
+import 'postcss-pxtorem'
+import './style/base.css'
+import Vant from 'vant' // vant 组件库
+import 'vant/lib/index.css' // vant 的样式
 
 const app = createApp(App)
 app.use(router)
+app.use(Vant)
 app.mount('#app')
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,9 @@
 <template>
-  主页
+  <van-button>124</van-button>
 </template>
+
+<style scoped>
+p {
+  width: 100px
+}
+</style>
